0f295d53b467938cfe8db9b213b18bb227bd8881
Y24 <githuby24@gmail.com>
2019-07-12 20:54:07 +0800

    Normal bug-fixing.

--- src/main/java/cn/org/y24/EmploySystem/config/WebSecurityConfig.java
+++ src/main/java/cn/org/y24/EmploySystem/config/WebSecurityConfig.java
@@ -20,7 +20,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.authorizeRequests().antMatchers("/", "/home,/index").permitAll().and().formLogin();
     }
 
     @Override
