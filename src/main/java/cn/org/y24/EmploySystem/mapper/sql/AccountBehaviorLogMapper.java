0da96a377f39a316c252e9295ba7fd89724f4f02
RainOfShadow <986352486@qq.com>
2021-09-07 11:44:08 +0800

    .

--- src/main/java/cn/org/y24/EmploySystem/mapper/sql/AccountBehaviorLogMapper.java
+++
@@ -1,8 +0,0 @@
-package cn.org.y24.EmploySystem.mapper.sql;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface AccountBehaviorLogMapper {
-
-}
--- src/main/java/com/group/employsystem/EmploySystemApplication.java
+++ src/main/java/com/group/employsystem/EmploySystemApplication.java
@@ -1,4 +1,4 @@
-package cn.org.y24.EmploySystem;
+package com.group.employsystem;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
-@MapperScan(value = {"cn.org.y24.EmploySystem.mapper"})
 public class EmploySystemApplication {
 
     public static void main(String[] args) {
